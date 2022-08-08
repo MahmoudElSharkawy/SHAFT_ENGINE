5d56ef85b58b065f3740129c555124a6f0eceb03
Mahmoud El-Sharkawy <mahmoud.elsharkawy1994@gmail.com>
2022-08-08 20:18:47 +0200

    Fix the JSON path data type value bug

--- src/main/java/com/shaft/api/RestActions.java
+++ src/main/java/com/shaft/api/RestActions.java
@@ -175,8 +175,8 @@ public class RestActions {
                 List<String> jsonValueAsList = JsonPath.read(response.asPrettyString(), jsonPath);
                 searchPool = String.valueOf(jsonValueAsList.get(0));
             } else {
-                searchPool = JsonPath.read(response.asPrettyString(), jsonPath);
-            }
+                var jsonValue = JsonPath.read(response.asPrettyString(), jsonPath);
+                searchPool = String.valueOf(jsonValue);            }
         } catch (ClassCastException rootCauseException) {
             ReportManager.log(ERROR_INCORRECT_JSONPATH + "\"" + jsonPath + "\"");
             failAction(jsonPath, rootCauseException);
@@ -205,7 +205,8 @@ public class RestActions {
                     List<String> jsonValueAsList = JsonPath.read(responseObject.asPrettyString(), jsonPath);
                     searchPool = String.valueOf(jsonValueAsList.get(0));
                 } else {
-                    searchPool = JsonPath.read(responseObject.asPrettyString(), jsonPath);
+                    var jsonValue = JsonPath.read(responseObject.asPrettyString(), jsonPath);
+                    searchPool = String.valueOf(jsonValue);
                 }
             }
         } catch (ClassCastException rootCauseException) {
